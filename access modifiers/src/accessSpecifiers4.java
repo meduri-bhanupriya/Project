b7d2208f10f6bb575121c5466466b2e68f5d1f51
Meduri Bhanu priya <meduribhanupriya18@gmail.com>
2022-02-28 12:49:24 +0000

    EmailValidation commit

---
+++ access modifiers/src/accessSpecifiers4.java
@@ -0,0 +1,25 @@
+package pack1;
+
+public class pubaccessspecifiers {
+
+	public void display() 
+    { 
+        System.out.println("This is Public Access Specifiers"); 
+    } 
+}
+
+//create another package
+package pack2;
+import pack1.*;
+
+public class accessSpecifiers4 {
+
+	public static void main(String[] args) {
+		
+		pubaccessspecifiers obj = new pubaccessspecifiers(); 
+        obj.display();  
+		
+	}
+}
+
+
