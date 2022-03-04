0ffb72439a6a47e84c0c872b8e00463314486178
Meduri Bhanu priya <meduribhanupriya18@gmail.com>
2022-03-04 11:32:06 +0000

    Method Overloading commit

---
+++ Methods/src/methodOverloading.java
@@ -0,0 +1,20 @@
+
+public class methodOverloading {
+	
+public void area(int b,int h)
+    {
+         System.out.println("Area of Triangle : "+(0.5*b*h));
+    }
+    public void area(int r) 
+    {
+         System.out.println("Area of Circle : "+(3.14*r*r));
+    }
+
+    public static void main(String args[])
+   {
+
+methodOverloading ob=new methodOverloading();
+       ob.area(5,5);
+       ob.area(10);  
+   }
+}
