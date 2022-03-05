f3b04f82bee9c3b2be99a93eac8b4311d7c8f0a4
Meduri Bhanu priya <meduribhanupriya18@gmail.com>
2022-03-05 18:14:02 +0000

    Insertion Sort commit

---
+++ InsertionSort/src/insertionSort.java
@@ -0,0 +1,28 @@
+public class insertionSort {
+
+    public static  void main(String[] args){
+
+        int[] arr = {111,22,45,1,82,0,36,91};
+        insertionSort(arr);
+        for(int i=0;i<arr.length;i++){
+
+            System.out.println(arr[i]);
+
+        }
+     }
+    public static void insertionSort(int[] arr){
+
+    int len = arr.length;
+    for(int j=1;j<len;j++){
+    int key = arr[j];
+    int i=j-1;
+    while ((i>-1) && (arr[i]>key)){
+
+        arr[i+1]=arr[i];
+        i--;
+    }
+    arr[i+1]=key;
+         }
+
+    }
+}
