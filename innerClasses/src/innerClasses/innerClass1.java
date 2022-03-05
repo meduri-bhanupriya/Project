c360853dcef19618387689c835b20bcb9d0fb2a7
Meduri Bhanu priya <meduribhanupriya18@gmail.com>
2022-03-05 09:26:05 +0000

    Inner Classes commit

---
+++ innerClasses/src/innerClasses/innerClass1.java
@@ -0,0 +1,18 @@
+package innerClasses;
+
+public class innerClass1 {
+
+	 private String msg="Welcome to Java"; 
+	 
+	 class Inner{  
+	  void hello(){System.out.println(msg+", Let's start learning Inner Classes");}  
+	 }  
+
+
+	public static void main(String[] args) {
+
+		innerClass1 obj=new innerClass1();
+		innerClass1.Inner in=obj.new Inner();  
+		in.hello();  
+	}
+}
