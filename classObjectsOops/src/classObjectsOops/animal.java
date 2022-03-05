d8b4d7c5738c19b8208c17c71ee2e71d2b39e233
Meduri Bhanu priya <meduribhanupriya18@gmail.com>
2022-03-05 16:32:28 +0000

    Class,Objects,OOPS commit

---
+++ classObjectsOops/src/classObjectsOops/animal.java
@@ -0,0 +1,42 @@
+package classObjectsOops;
+
+public class animal
+{  
+    String name; 
+    String breed; 
+    int age; 
+    String color; 
+    public animal(String name, String breed, int age, String color) 
+    { 
+        this.name = name; 
+        this.breed = breed; 
+        this.age = age; 
+        this.color = color; 
+    } 
+    public String getName() 
+    { 
+        return name; 
+    } 
+    public String getBreed() 
+    { 
+        return breed; 
+    } 
+    public int getAge() 
+    { 
+        return age; 
+    } 
+    public String getColor()                                                                    
+    { 
+        return color; 
+    } 
+    @Override
+    public String toString() 
+    { 
+        return("Hi my name is "+ this.getName()+ ".\nMy breed,age and color are " + this.getBreed()+", " + this.getAge()+ ", and"+ this.getColor() + "."); 
+    } 
+    public static void main(String[] args) 
+    { 
+    	animal snapo = new animal("Snapo","puppy", 5, "brown"); 
+        System.out.println(snapo.toString()); 
+    } 
+}
