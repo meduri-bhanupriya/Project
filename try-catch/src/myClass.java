8c49f6dc9bb69d554924861c57bd0125a8d0572c
Meduri Bhanu priya <meduribhanupriya18@gmail.com>
2022-03-05 13:57:41 +0000

    Try-Catch commit

---
+++ try-catch/src/myClass.java
@@ -0,0 +1,20 @@
+
+public class myClass 
+{
+    public static void main(String args[]) 
+    {
+        int[] array = new int[5];
+        try 
+        {
+            array[6] = 3;
+        }
+        catch (ArrayIndexOutOfBoundsException e) 
+        {
+            System.out.println("Array index is out of bounds!"); 
+        }
+        finally 
+        {
+            System.out.println("The array is of size " + array.length);
+        }
+    }
+}
